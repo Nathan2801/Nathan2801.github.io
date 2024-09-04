f4095680eefe219c1d7c8bd4b73ca5bac014a6d9
Nathan2801 <john.2801.oliver@gmail.com>
2024-09-03 23:27:28 -0300

    Should be adding useful information instead of playing with superficial styling...

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const Page = {
+    pages: (n) => n === undefined
+        ? document.body.children
+        : document.body.children[n],
 
+    middleDistance: (pageNr) => {
+        const y = window.scrollY;
+        const h = window.innerHeight;
+        return Math.abs(h*pageNr + h*0.5 - (y + h*0.5));
+    },
+
+    update: () => {
+        for (let i = 0; i < Page.pages().length; i++) {
+            const page = Page.pages(i);
+            const distance = Page.middleDistance(i);
+            if (distance > window.innerHeight) {
+                page.style.opacity = "0%";
+            } else {
+                const percentage = 100 - distance/window.innerHeight*100 + 50;
+                page.style.opacity = percentage + "%";
+            }
+        }
+    },
+};
+
+const main = () => {
+    document.addEventListener("scroll", (_) => Page.update());
+};
+
+main();
